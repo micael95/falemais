6ba7787b8b747401cb147a0d8ae1cc28e1d1a717
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-24 15:19:04 -0300

    test: ensure SignUpController returns 400 if validation fails.

---
+++ src/test/java/br/com/uaubox/falemais/controllers/SignUpControllerTests.java
@@ -0,0 +1,36 @@
+package br.com.uaubox.falemais.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class SignUpControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn400IfValidationReturnsAnError() throws Exception {
+        URI uri = new URI("/api/v1/signup");
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+}
