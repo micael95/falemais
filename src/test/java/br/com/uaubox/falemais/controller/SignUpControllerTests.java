e7c664302445a2a9d8f520fb08f39702c1714340
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-24 19:50:11 -0300

    test: should return 422 if password confirmation is invalid

--- src/test/java/br/com/uaubox/falemais/controller/SignUpControllerTests.java
+++ src/test/java/br/com/uaubox/falemais/controller/SignUpControllerTests.java
@@ -1,5 +1,8 @@
 package br.com.uaubox.falemais.controller;
 
+import br.com.uaubox.falemais.dto.request.CustomerRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +25,35 @@ import java.net.URI;
 @RunWith(SpringRunner.class)
 public class SignUpControllerTests {
 
+    private static final String SIGNUP_URI = "/api/v1/signup";
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    private Faker faker = new Faker();
 
     @Test
     public void shouldReturn400IfValidationReturnsAnError() throws Exception {
-        URI uri = new URI("/api/v1/signup");
+        URI uri = new URI(SIGNUP_URI);
         mockMvc.perform(MockMvcRequestBuilders
                 .post(uri)
                 .content("{}")
                 .contentType(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void shouldReturn422IfPasswordConfirmationIsInvalid() throws Exception {
+        URI uri = new URI(SIGNUP_URI);
+        CustomerRequest customerRequest = new CustomerRequest();
+        customerRequest.setName(faker.name().name());
+        customerRequest.setEmail(faker.internet().emailAddress());
+        customerRequest.setPassword(faker.internet().password());
+        customerRequest.setPasswordConfirmation("invalid_password");
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(objectMapper.writeValueAsString(customerRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
     }
 }
