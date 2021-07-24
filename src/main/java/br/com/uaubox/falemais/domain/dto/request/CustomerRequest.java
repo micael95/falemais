0a91d99abede38179152804f7bb91809c1cd2703
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-24 17:43:08 -0300

    feat: add customer request

---
+++ src/main/java/br/com/uaubox/falemais/domain/dto/request/CustomerRequest.java
@@ -0,0 +1,41 @@
+package br.com.uaubox.falemais.domain.dto.request;
+
+public class CustomerRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String passwordConfirmation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+}
