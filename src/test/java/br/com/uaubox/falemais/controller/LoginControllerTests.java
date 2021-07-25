e89092e5b4d7a4752c250fb2cf802fa250435133
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-25 02:27:41 -0300

    test: ensure LoginController returns 400 if invalid data is provided

---
+++ src/test/java/br/com/uaubox/falemais/controller/LoginControllerTests.java
@@ -0,0 +1,39 @@
+package br.com.uaubox.falemais.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class LoginControllerTests {
+
+    private static final String LOGIN_URI = "/oauth/token";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn400IfValidationReturnsAnError() throws Exception {
+        URI uri = new URI(LOGIN_URI);
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content("{}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+}
