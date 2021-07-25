0bab968db6bcd193863402c3f55c27eb8cbcc5e6
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-25 14:44:10 -0300

    test: ensure LoginController returns 200 if valid credentials is provided.

--- src/test/java/br/com/uaubox/falemais/controller/LoginControllerTests.java
+++ src/test/java/br/com/uaubox/falemais/controller/LoginControllerTests.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -68,6 +69,28 @@ public class LoginControllerTests {
                 .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 
+    @Test
+    public void shouldReturn200IfCorrectCredentialsIsProvided() throws Exception {
+        URI uri = new URI(LOGIN_URI);
+        String password = faker.internet().password();
 
+        Customer customer = new Customer();
+        customer.setName(faker.name().name());
+        customer.setEmail(faker.internet().emailAddress());
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        customer.setPassword(bCryptPasswordEncoder.encode(password));
+
+        customerRepository.save(customer);
+
+        TokenRequest tokenRequest = new TokenRequest();
+        tokenRequest.setEmail(customer.getEmail());
+        tokenRequest.setPassword(password);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri)
+                .content(objectMapper.writeValueAsString(tokenRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 
 }
